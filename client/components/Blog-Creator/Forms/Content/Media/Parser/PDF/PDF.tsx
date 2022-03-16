be4d6c5fd9ebbbda8f6362e6bb868d6d6a535777
ratasorin <ratasorin0@gmail.com>
2022-03-16 14:37:25 +0200

    Fix PDF source being statically set

--- client/components/Blog-Creator/Forms/Content/Media/Parser/PDF/PDF.tsx
+++ client/components/Blog-Creator/Forms/Content/Media/Parser/PDF/PDF.tsx
@@ -4,16 +4,13 @@ import pdf__styles from './pdf.module.css';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const PDF: FC<{ src: string }> = ({ src }) => {
-  const [numPages, setNumPages] = useState<number>(0);
+  const [numPages, setNumPages] = useState(0);
   const [pageNumber] = useState(1);
   function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
     setNumPages(numPages);
   }
   return (
-    <Document
-      file={`/api/images/user-draft01/Exercitii.pdf`}
-      onLoadSuccess={onDocumentLoadSuccess}
-    >
+    <Document file={src} onLoadSuccess={onDocumentLoadSuccess}>
       <Page width={500} pageNumber={pageNumber} />
     </Document>
   );
