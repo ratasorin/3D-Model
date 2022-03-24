d52e6e10d26d4a1cca20107fe2004ec5623d3376
ratasorin <ratasorin0@gmail.com>
2022-03-24 19:43:20 +0200

    Add travel feature on coordinates click.

--- client/components/Searchbox/SearchResults/Card/Section/Position/Position.tsx
+++ client/components/Searchbox/SearchResults/Card/Section/Position/Position.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import Section from '../Section';
 import { HiLocationMarker } from 'react-icons/hi';
 import position_style from './position.module.css';
+import { coordinates } from 'components/Map/Map';
 const Position: FC<{ x: number; y: number }> = ({ x, y }) => {
   return (
     <Section
@@ -12,7 +13,7 @@ const Position: FC<{ x: number; y: number }> = ({ x, y }) => {
       }}
       mainContent={{
         sideEffects: () => {
-          //
+          coordinates.next([x, y]);
         },
         element: (
           <>
