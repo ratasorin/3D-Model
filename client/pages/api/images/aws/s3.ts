7543285951201f5775bafc5e54f5582c9ec7186f
ratasorin <ratasorin0@gmail.com>
2022-04-23 19:46:50 +0300

    Fix all environment variables for S3

--- client/pages/api/images/aws/s3.ts
+++ client/pages/api/images/aws/s3.ts
@@ -3,8 +3,8 @@ import S3 from 'aws-sdk/clients/s3';
 const s3 = new S3({
   region: process.env.AWS_REGION_NAME,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY as string,
+    secretAccessKey: process.env.AWS_S3_SECRET_ACCESS_KEY as string,
   },
 });
 
