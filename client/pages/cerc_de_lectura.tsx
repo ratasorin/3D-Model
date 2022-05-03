2f5b3ac3bfba2255a22e4b781f4af5d7f672ad31
ratasorin <ratasorin0@gmail.com>
2022-05-03 14:23:53 +0300

    Add blog visualizer on "cerc-de-lectura"

--- client/pages/cerc_de_lectura.tsx
+++ client/pages/cerc_de_lectura.tsx
@@ -1,5 +1,6 @@
 import Header from 'components/Blog-Creator/Header/Header';
 import Dispatch from 'components/Widgets/Button/Dispatch/Dispatch';
+import Blog from 'components/Widgets/Modals/Blogs/Blog/Blog';
 import Card from 'components/Widgets/Modals/Blogs/Card/Card';
 import { useBlogs } from 'hooks/useBlogs';
 import { useRouter } from 'next/router';
@@ -10,34 +11,37 @@ const CercDeLectura = () => {
   const blogs = useBlogs('cerc de lectura');
   const router = useRouter();
   return (
-    <div className={cerc__style.container}>
-      <Header
-        monument="Cerc de lectura"
-        subtitle="Arhiva culturala"
-        Button={
-          <Dispatch
-            action={() => {
-              router.push(`create-blog/Cerc de lectura`);
-            }}
-            payload="SCRIETI O POSTARE"
-          />
-        }
-      />
-      {blogs?.map((blog) => (
-        <Card
-          authorID={blog.userId}
-          blogID={blog.blogId}
-          description={descriptionFrom(blog.content)}
-          date={dateFrom(blog.createdAt)}
-          likes={blog.likeCount}
-          monument="cerc de lectura"
-          rawContent={blog.content}
-          key={blog.content}
-          title={blog.title}
-          golden={false}
+    <>
+      <div className={cerc__style.container}>
+        <Header
+          monument="Cerc de lectura"
+          subtitle="Arhiva culturala"
+          Button={
+            <Dispatch
+              action={() => {
+                router.push(`create-blog/Cerc de lectura`);
+              }}
+              payload="SCRIETI O POSTARE"
+            />
+          }
         />
-      ))}
-    </div>
+        {blogs?.map((blog) => (
+          <Card
+            authorID={blog.userId}
+            blogID={blog.blogId}
+            description={descriptionFrom(blog.content)}
+            date={dateFrom(blog.createdAt)}
+            likes={blog.likeCount}
+            monument="cerc de lectura"
+            rawContent={blog.content}
+            key={blog.content}
+            title={blog.title}
+            golden={false}
+          />
+        ))}
+      </div>
+      <Blog />
+    </>
   );
 };
 
