ddbe5c644edd41bb9113826a82f39dec59d1c273
ratasorin <ratasorin0@gmail.com>
2022-05-11 19:42:01 +0300

    Switch to NextImages for better First Contentful Paint

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,24 +1,27 @@
-import index__style from './index.module.css';
+import landing__style from './index.module.css';
 import Link from 'next/link';
 import { normalizePath } from 'utils/normalize-path';
+import Image from 'next/image';
 const sections = ['HARTA', 'CERC DE LECTURA', 'GALERIE'] as const;
 
 const Index = () => {
   return (
-    <div className={index__style.container}>
+    <div className={landing__style.container}>
       {sections.map((section) => (
-        <div
-          key={section}
-          className={index__style.section}
-          style={{
-            backgroundImage: `url("${normalizePath(
-              section.toLowerCase()
-            )}.jpg")`,
-          }}
-        >
+        <div key={section} className={landing__style.section}>
           <Link href={`${normalizePath(section.toLowerCase())}`}>
-            <a className={index__style.section__a}>{section}</a>
+            <a className={landing__style.section__a}>{section}</a>
           </Link>
+          <Image
+            src={`/${normalizePath(section.toLowerCase())}.jpg`}
+            className={landing__style.image}
+            blurDataURL={`/${normalizePath(section.toLowerCase())}.jpg`}
+            placeholder="blur"
+            layout="fill"
+            objectFit="cover"
+            priority
+            quality={100}
+          />
         </div>
       ))}
     </div>
