dec2226184d891d4cd1cc4f680f1efa3e6b1fbb8
ratasorin <ratasorin0@gmail.com>
2022-05-11 14:10:21 +0300

    Fix "cerc de lectura" blogs

--- client/pages/cerc_de_lectura/index.tsx
+++ client/pages/cerc_de_lectura/index.tsx
@@ -1,16 +1,47 @@
+import { Blogs } from '@prisma/client';
 import Header from 'components/Blog-Creator/Header/Header';
 import Loading from 'components/Loading/Loading';
 import Dispatch from 'components/Widgets/Button/Dispatch/Dispatch';
+import { setBlogsForSubject } from 'components/Widgets/Modals/Blogs/blog-posts-slice';
 import Blog from 'components/Widgets/Modals/Blogs/Blog/Blog';
 import Card from 'components/Widgets/Modals/Blogs/Card/Card';
-import { useBlogs } from 'hooks/useBlogs';
+import { useAppDispatch, useAppSelector } from 'hooks/redux-hooks';
 import { useRouter } from 'next/router';
+import { useCallback, useEffect, useState } from 'react';
+import { ServerResponse } from 'types/server';
 import { dateFrom } from 'utils/date';
 import { descriptionFrom } from 'utils/description-from-content';
 import cerc__style from './cerc-de-lectura.module.css';
+
+const NAME = 'cerc de lectura';
 const CercDeLectura = () => {
-  const { blogs, loading } = useBlogs('cerc de lectura');
   const router = useRouter();
+  const blogs = useAppSelector(({ blogPosts }) => blogPosts[NAME]);
+  const fetchBlogs = useCallback(async () => {
+    const response = await fetch(`/api/blogs/get-blogs/${NAME}`);
+    return (await response.json()) as ServerResponse<Blogs[]>;
+  }, []);
+
+  const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const useBlogs = async () => {
+      if (!blogs) {
+        setLoading(true);
+        const response = await fetchBlogs();
+        if (response.error) return;
+        dispatch(
+          setBlogsForSubject({
+            blogs: response.payload,
+            subject: NAME,
+          })
+        );
+        setLoading(false);
+      }
+    };
+
+    useBlogs();
+  }, [blogs]);
   return (
     <>
       <div className={cerc__style.container}>
