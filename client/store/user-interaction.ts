5671c8094ef99ffd00909a6c0c6093e8f1702e70
ratasorin <ratasorin0@gmail.com>
2022-05-11 19:17:40 +0300

    Overwrite changes

--- client/store/user-interaction.ts
+++ client/store/user-interaction.ts
@@ -1,10 +1,11 @@
 import showSearchReducer from 'components/Searchbox/search-slice';
 import changeInfoReducer from 'components/Widgets/Modals/Modify/Field/info-slice';
 import titleReducer from 'components/Blog-Creator/Forms/Title/title-slice';
-import blogPostsReducer from 'components/Widgets/Modals/Blogs/blog-posts-slice';
+import blogPosts from 'components/Widgets/Modals/Blogs/blog-posts-slice';
+
 export const userInteractionReducers = {
   showSearch: showSearchReducer,
   changeInfo: changeInfoReducer,
   title: titleReducer,
-  blogPosts: blogPostsReducer,
+  blogPosts,
 };
