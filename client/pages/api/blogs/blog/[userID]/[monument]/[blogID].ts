f1737ff5fad246f58f98e34dcd398241b33a41da
ratasorin <ratasorin0@gmail.com>
2022-05-11 14:20:33 +0300

    Eliminate shadow database

--- client/pages/api/blogs/blog/[userID]/[monument]/[blogID].ts
+++ client/pages/api/blogs/blog/[userID]/[monument]/[blogID].ts
@@ -1,8 +1,5 @@
 import { NextApiRequest } from 'next';
-// import { ErrorResponse } from 'types/server';
-// import parse from 'utils/normalize-path';
 import prisma from 'utils/prisma';
-
 export default async function (req: NextApiRequest) {
   const blogID = req.query.blogID as string;
   try {
